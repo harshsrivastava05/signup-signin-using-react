4f3d9e8e97c6acb9aaecca77b8fab4950a3d1c84
harsh srivastava <130855160+harshsrivastava05@users.noreply.github.com>
2023-08-09 15:32:32 +0530

    Update App.jsx

--- course-selling/src/App.jsx
+++ course-selling/src/App.jsx
@@ -1,26 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Appbar from "./appbar.jsx";
 import Signin from "./signin.jsx";
+import Addcourse from "./addcourse.jsx";
 import Signup from "./signup.jsx";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div style={{
-      height: '100vh',
-      width: "100vw",
-      backgroundColor: "#eeeeee"
-    }}>
-      <Appbar></Appbar>
-      <Router>
-          <Routes>
-              <Route path="/signin" element={<Signin />} />
-              <Route path="/signup" element={<Signup />} />
-              
-             
-          </Routes>
-      </Router>
-      </div>
-  );
+
+    return (
+        <div style={{width: "100vw",
+            height: "100vh",
+            backgroundColor: "#eeeeee"}}
+        >
+            <Router>
+                <Appbar />
+                <Routes>
+                <Route path="/admin/courses" element={<Addcourse />} />
+              <Route path="/admin/signin" element={<Signin />} />
+              <Route path="/admin/signup" element={<Signup />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
-export default App
+export default App;
