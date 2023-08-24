d9afc3ec25c523b1858d00d937f37371eba270c8
harsh srivastava <130855160+harshsrivastava05@users.noreply.github.com>
2023-08-24 19:00:18 +0530

    Update App.jsx

--- course-selling/src/App.jsx
+++ course-selling/src/App.jsx
@@ -3,6 +3,9 @@ import Appbar from "./appbar.jsx";
 import Signin from "./signin.jsx";
 import Addcourse from "./addcourse.jsx";
 import Signup from "./signup.jsx";
+import Courses from './course.jsx';
+import CourseId from './CourseId.jsx';
+
 
 function App() {
 
@@ -14,9 +17,12 @@ function App() {
             <Router>
                 <Appbar />
                 <Routes>
+                <Route path="/admin/signup" element={<Signup />} />
+                <Route path="/" element={<Signup />} />
+                <Route path="/admin/signin" element={<Signin />} />
                 <Route path="/admin/addcourse" element={<Addcourse />} />
-              <Route path="/admin/signin" element={<Signin />} />
-              <Route path="/admin/signup" element={<Signup />} />
+                <Route path="/admin/course" element={<Courses />} />
+                <Route path="/admin/course/:courseId" element={<CourseId />} />
                 </Routes>
             </Router>
         </div>
